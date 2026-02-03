bb2ca678671754245ced78c35d4eca6f462ee9b8
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-03 17:34:53 +0600

    Choosing the State Structure - Group related state-pointer-red

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./component/Form";
+
+function App() {
+  return (
+    <div className="">
+      {/* <Pointer></Pointer> */}
+      <Form />
+    </div>
+  );
+}
+
+export default App;
