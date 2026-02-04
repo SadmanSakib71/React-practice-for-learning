481b0df6d2046e1ea5d85e66ef7728e8c3eb5ae1
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-04 17:23:40 +0600

    lifted state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import Travel from "./component/Travel";
+import { useState } from "react";
+import Accordion from "./component/Accordion";
 
 function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="">
-      <Travel />
+      <Accordion
+        isActive={activeIndex === 0}
+        onClick={() => setActiveIndex(0)}
+        title="sakib"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        praesentium. Explicabo consequuntur quae iste, voluptates fugiat quos
+        distinctio optio vel molestiae obcaecati esse dolorem enim voluptas
+        alias quam aliquam magni.
+      </Accordion>
+      <Accordion
+        isActive={activeIndex === 1}
+        onClick={() => setActiveIndex(1)}
+        title="Any"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        praesentium. Explicabo consequuntur quae iste, voluptates fugiat quos
+        distinctio optio vel molestiae obcaecati esse dolorem enim voluptas
+        alias quam aliquam magni.
+      </Accordion>
     </div>
   );
 }
