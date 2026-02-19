5790c78f8e5c935c5c8b5f38229a5b05795f5f73
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-19 15:55:32 +0600

    before-flashSync

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import "./App.css";
-import Heading from "./component/Heading";
-import Section from "./component/Section";
+import TodoList from "./component/TodoList";
 
 const App = () => {
   return (
-    <Section level={1}>
-      <Heading>Heading</Heading>
-      <Section level={2}>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Heading>Heading</Heading>
-        <Section level={3}>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Heading>Sub-heading</Heading>
-          <Section level={4}>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-            <Heading>Sub-sub-heading</Heading>
-          </Section>
-        </Section>
-      </Section>
-    </Section>
+    <>
+      <TodoList />
+    </>
   );
 };
 
