9bf1ddda9046eadf1aed5b61a87d73ebcae432c4
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-19 15:56:36 +0600

    after using flashSync for scrollOnView

--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import { flushSync } from "react-dom";
 
 const TodoList = () => {
   const listRef = useRef(null);
@@ -8,7 +9,10 @@ const TodoList = () => {
   function handleAdd() {
     const newTodo = { id: nextId++, text: text };
     setText("");
-    setTodos([...todos, newTodo]);
+
+    flushSync(() => {
+      setTodos([...todos, newTodo]);
+    });
 
     listRef.current.lastChild.scrollIntoView({
       behavior: "smooth",
