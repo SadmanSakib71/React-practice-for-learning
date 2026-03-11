4bca3c87ccfb31f6d90368526531610ba5dcf2f5
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-03-11 14:17:52 +0600

    making custom hook for two component

--- src/component/StatusBar.jsx
+++ src/component/StatusBar.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 const StatusBar = () => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  const handleOnline = () => {
+    setIsOnline(true);
+  };
+
+  const handleOffline = () => {
+    setIsOnline(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
+  }, []);
+
   return (
     <>
-      <h1>✅ Online or ❌ Disconnected</h1>
+      <h1>{isOnline ? "✅ Online" : "❌ Disconnected"}</h1>
     </>
   );
 };
